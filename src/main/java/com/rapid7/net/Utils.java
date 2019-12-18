07d57bd6665c2c1ce48e55be8f9982089b1b4bc8
Philip Helger <philip@helger.com>
2019-12-18 17:00:53 +0100

    Added comments

--- src/main/java/com/rapid7/net/Utils.java
+++ src/main/java/com/rapid7/net/Utils.java
@@ -1,6 +1,14 @@
 package com.rapid7.net;
 
 final class Utils {
+  // private constructor
+  private Utils (){}
+  
+  /**
+   * Check if the contained string is <code>null</code> or empty. Trimming is NOT applied.
+   * @param s The String to be checked.
+   * @return <code>true</code> if the passed String is <code>null</code> or empty.
+   */
   public static boolean isNullOrEmpty (String s){
     return s == null || s.length () == 0;
   }

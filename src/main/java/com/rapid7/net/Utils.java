3aa0a4bfe88fc1c2413fb0bfb34c4846f6a14d6b
Philip Helger <philip@helger.com>
2019-12-19 09:01:49 +0100

    "final" String; improved docs

--- src/main/java/com/rapid7/net/Utils.java
+++ src/main/java/com/rapid7/net/Utils.java
@@ -1,15 +1,20 @@
 package com.rapid7.net;
 
+/**
+ * Utility class to avoid including external dependencies.
+ * 
+ * @since 1.1.41
+ */
 final class Utils {
   // private constructor
   private Utils (){}
   
   /**
    * Check if the contained string is <code>null</code> or empty. Trimming is NOT applied.
-   * @param s The String to be checked.
+   * @param s The String to be checked. May be <code>null</code>.
    * @return <code>true</code> if the passed String is <code>null</code> or empty.
    */
-  public static boolean isNullOrEmpty (String s){
+  public static boolean isNullOrEmpty (final String s){
     return s == null || s.length () == 0;
   }
 }
